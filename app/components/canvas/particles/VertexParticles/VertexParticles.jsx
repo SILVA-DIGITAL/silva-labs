1557ad3fa039e1595126e0da49e87b5461fc6bc2
Maximillian <maxsilvauk@gmail.com>
2024-02-29 12:54:51 +0000

    Vertex particles component

---
+++ app/components/canvas/particles/VertexParticles/VertexParticles.jsx
@@ -0,0 +1,52 @@
+import * as THREE from 'three'
+import { useMemo } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { vertexShader } from '@/shaders/vertexShader'
+import { fragmentShader } from '@/shaders/fragmentShader'
+
+const VertexParticles = () => {
+  const planePositions = useMemo(() => {
+    const planeGeometry = new THREE.PlaneGeometry(6, 6, 128, 128)
+    const positions = planeGeometry.attributes.position.array
+
+    return positions
+  }, [])
+
+  const shaderArgs = useMemo(
+    () => ({
+      uniforms: {
+        uTime: { value: 0 },
+      },
+      vertexShader,
+      fragmentShader,
+    }),
+    []
+  )
+
+  useFrame(() => {
+    shaderArgs.uniforms.uTime.value++
+  }, [])
+
+  return (
+    <>
+      <points rotation={[-Math.PI / 2, 0, 0]}>
+        <bufferGeometry attach='geometry'>
+          <bufferAttribute
+            attach='attributes-position'
+            array={planePositions}
+            itemSize={3}
+            count={planePositions.length / 3}
+          />
+        </bufferGeometry>
+        <shaderMaterial
+          args={[shaderArgs]}
+          transparent
+          depthTest={false}
+          depthWrite={false}
+        />
+      </points>
+    </>
+  )
+}
+
+export default VertexParticles
---
+++ app/components/canvas/particles/VertexParticles/index.js
@@ -0,0 +1,1 @@
+export { default as VertexParticles } from './VertexParticles'
